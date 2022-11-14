8b20a29bdbe22f2f2ae15dd38586fa9a681c2a8e
M3chowski <k4rrrlson@gmail.com>
2022-11-14 12:19:01 +0400

    mr changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Logo } from "./logo";
+import { Search } from "./search";
 
 import "./header.css";
 
 export const Header = () => {
   return (
     <div className="header">
-      <div className="header-left">start</div>
+      <div className="header-left">
+        <Logo />
+        <Search />
+      </div>
     </div>
   );
 };
