8effade6b6e7f389722869ff25e7030f830e0c3b
M3chowski <k4rrrlson@gmail.com>
2022-11-14 09:09:55 +0400

    mch

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { CityChanger } from "./city-changer";
-import { Logo } from "./logo";
-import { WorkTime } from "./work-time/Work-Time";
 
 import "./header.css";
 
 export const Header = () => {
   return (
     <div className="header">
-      <Logo />
-      <CityChanger />
-      <WorkTime />
+      <div className="header-left">start</div>
     </div>
   );
 };
